69980ff51d51781fcfa3f02b187c381ecd10f8aa
Jordan D Disch <jordanddisch@Jordans-MacBook-Air.local>
2017-06-24 16:31:27 -0600

    removed rm -rf

--- scripts/deploy-ghpages.sh
+++ scripts/deploy-ghpages.sh
@@ -24,9 +24,7 @@ git pull origin master
 if git rev-parse --verify origin/gh-pages > /dev/null 2>&1
 then
     git checkout gh-pages
-    # delete any old site as we are going to replace it
-    # Note: this explodes if there aren't any, so moving it here for now
-    git rm -rf .
+    ls
 else
     git checkout --orphan gh-pages
 fi
