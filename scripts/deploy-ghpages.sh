22757991621b996aa29d21c06b95b16d0928300d
Jordan D Disch <jordanddisch@Jordans-MacBook-Air.local>
2017-06-25 13:43:11 -0600

    removed if statement

--- scripts/deploy-ghpages.sh
+++ scripts/deploy-ghpages.sh
@@ -7,18 +7,32 @@
 # abort the script if there is a non-zero error
 set -e
 
-git checkout gh-pages
-git reset --hard HEAD
-git rm -rf .
-git clean -df
-git status
+# show where we are on the machine
+pwd
+remote=$(git config remote.origin.url)
+
+# make a directory to put the gp-pages branch
+mkdir gh-pages-branch
+cd gh-pages-branch
+# now lets setup a new repo so we can update the gh-pages branch
+git config --global user.email "$GH_EMAIL" > /dev/null 2>&1
+git config --global user.name "$GH_NAME" > /dev/null 2>&1
+git init
+git remote add --fetch origin "$remote"
 git pull origin master
 
+git checkout -b gh-pages
+ls
+
 # stage any changes and new files
-git add .
+git add -A
 # now commit, ignoring branch gh-pages doesn't seem to work, so trying skip
 git commit --allow-empty -m "Deploy to GitHub pages [ci skip]"
 # and push, but send any output to /dev/null to hide anything sensitive
 git push --force --quiet origin gh-pages
+# go back to where we started and remove the gh-pages git repo we made and used
+# for deployment
+cd ..
+rm -rf gh-pages-branch
 
 echo "Finished Deployment!"
