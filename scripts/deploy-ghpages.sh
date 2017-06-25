f880ee8266c61b1609a3b3b960c1e720cf912627
Jordan D Disch <jordanddisch@Jordans-MacBook-Air.local>
2017-06-25 12:36:36 -0600

    added original shell script

--- scripts/deploy-ghpages.sh
+++ scripts/deploy-ghpages.sh
@@ -1,14 +1,21 @@
-deas used from https://gist.github.com/motemen/8595451
-
-# Based on https://github.com/eldarlabs/ghpages-deploy-script/blob/master/scripts/deploy-ghpages.sh
-# Used with their MIT license https://github.com/eldarlabs/ghpages-deploy-script/blob/master/LICENSE
+#!/bin/sh
+# ideas used from https://gist.github.com/motemen/8595451
 
 # abort the script if there is a non-zero error
 set -e
 
 # show where we are on the machine
 pwd
+
 remote=$(git config remote.origin.url)
+
+siteSource="$1"
+
+if [ ! -d "$siteSource" ]
+then
+    echo "Usage: $0 <site source dir>"
+    exit 1
+fi
 
 # make a directory to put the gp-pages branch
 mkdir gh-pages-branch
@@ -23,22 +30,23 @@ git remote add --fetch origin "$remote"
 if git rev-parse --verify origin/gh-pages > /dev/null 2>&1
 then
     git checkout gh-pages
-    git pull origin master
-    ls
+    # delete any old site as we are going to replace it
+    # Note: this explodes if there aren't any, so moving it here for now
+    git rm -rf .
 else
     git checkout --orphan gh-pages
-    ls
 fi
 
 # copy over or recompile the new site
-#cp -a "../${siteSource}/." .
+cp -a "../${siteSource}/." .
 
 # stage any changes and new files
 git add -A
 # now commit, ignoring branch gh-pages doesn't seem to work, so trying skip
 git commit --allow-empty -m "Deploy to GitHub pages [ci skip]"
 # and push, but send any output to /dev/null to hide anything sensitive
-git push --force --quiet origin gh-pages
+git push --force --quiet origin gh-pages > /dev/null 2>&1
+
 # go back to where we started and remove the gh-pages git repo we made and used
 # for deployment
 cd ..
