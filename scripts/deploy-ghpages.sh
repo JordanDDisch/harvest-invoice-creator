e5046a20cb2de36074f05ae3d1df7c57bec03941
Jordan D Disch <jordanddisch@Jordans-MacBook-Air.local>
2017-06-25 13:20:12 -0600

    Removed unecessary commands in shell script

--- scripts/deploy-ghpages.sh
+++ scripts/deploy-ghpages.sh
@@ -1,2 +1,23 @@
 #!/bin/sh
 # ideas used from https://gist.github.com/motemen/8595451
+
+# Based on https://github.com/eldarlabs/ghpages-deploy-script/blob/master/scripts/deploy-ghpages.sh
+# Used with their MIT license https://github.com/eldarlabs/ghpages-deploy-script/blob/master/LICENSE
+
+# abort the script if there is a non-zero error
+set -e
+
+cd harvest-invoice-creator
+
+git checkout gh-pages
+git rm -rf .
+git pull origin master
+
+# stage any changes and new files
+git add -A
+# now commit, ignoring branch gh-pages doesn't seem to work, so trying skip
+git commit --allow-empty -m "Deploy to GitHub pages [ci skip]"
+# and push, but send any output to /dev/null to hide anything sensitive
+git push --force --quiet origin gh-pages
+
+echo "Finished Deployment!"
