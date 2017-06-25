e72bd70d08b6a23973ea77ffa421870e531c932d
Jordan D Disch <jordanddisch@Jordans-MacBook-Air.local>
2017-06-25 13:05:43 -0600

    echoing ls

--- scripts/deploy-ghpages.sh
+++ scripts/deploy-ghpages.sh
@@ -25,6 +25,7 @@ git pull origin master
 if git rev-parse --verify origin/gh-pages > /dev/null 2>&1
 then
     git checkout gh-pages
+    echo ls
     # delete any old site as we are going to replace it
     # Note: this explodes if there aren't any, so moving it here for now
     git rm -rf .
